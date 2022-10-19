28bd4efa7669040b670ce04839d12ba61c482e42
MaoChongxin <chongxin.mao@shopee.com>
2022-10-19 15:12:49 +0800

    feature: drop table

--- src/observer/storage/common/db.cpp
+++ src/observer/storage/common/db.cpp
@@ -84,6 +84,22 @@ RC Db::create_table(const char *table_name, int attribute_count, const AttrInfo 
   return RC::SUCCESS;
 }
 
+RC Db::drop_table(const char* table_name) {
+  RC rc = RC::SUCCESS;
+
+  // table not found
+  if (!opened_tables_.count(table_name)) {
+    LOG_WARN("%s not found.", table_name);
+    return RC::SCHEMA_TABLE_NOT_EXIST;
+  }
+
+  auto table = opened_tables_[table_name];
+  delete table;
+  opened_tables_.erase(table_name);
+  LOG_INFO("Drop table success. table name=%s", table_name);
+  return rc;
+}
+
 Table *Db::find_table(const char *table_name) const
 {
   std::unordered_map<std::string, Table *>::const_iterator iter = opened_tables_.find(table_name);
